735a892a36a037059eba1031a497d73b488b3074
John Huang <john.huang@decentdao.org>
2025-03-17 15:36:40 -0700

    Remove generated JS

--- src/Governance.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TokenType = void 0;
-var TokenType;
-(function (TokenType) {
-    TokenType["ERC20"] = "ERC20";
-    TokenType["ERC721"] = "ERC721";
-    TokenType["ERC1155"] = "ERC1155";
-})(TokenType || (exports.TokenType = TokenType = {}));
--- src/Vote.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Vote = void 0;
-var Vote;
-(function (Vote) {
-    Vote["Yes"] = "YES";
-    Vote["No"] = "NO";
-    Vote["Abstain"] = "ABSTAIN";
-})(Vote || (exports.Vote = Vote = {}));
